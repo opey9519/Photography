75dfcec05915c92f0c80a2ed1c8061c0aedd8eec
Gavin Wilson <65029789+opey9519@users.noreply.github.com>
2024-08-04 13:06:00 -0400

    Add files via upload

---
+++ landing_page.js
@@ -0,0 +1,5 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    
+    document.getElementById('dropdown').style.display = document.getElementById('dropdown').style.display =='block' ?   'none' : 'block';
+}
